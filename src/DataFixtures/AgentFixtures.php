1b11070839e40c5197cbfc03a1235264390e69ab
radoA <rkajy@hotmail.fr>
2019-03-09 10:39:01 +0100

    maj migration bdd

--- src/DataFixtures/AgentFixtures.php
+++ src/DataFixtures/AgentFixtures.php
@@ -17,13 +17,54 @@ class AgentFixtures extends Fixture
 	}
     public function load(ObjectManager $manager)
     {
-       /* $agent = new Agent();
-        $agent->setLogin('testAgentAff');
-        $agent->setMdp($this->encoder->encodePassword($agent, 'testAgentAff'));
-        $agent->setNom('testNom');
-        $agent->setPrenom('testPrenom');
-		$agent->setAdresse('testAdresse');
-        $manager->persist($agent);
-        $manager->flush();*/
+        
+        
+        $agentTest = new Agent();
+        $agentTest->setLogin('loginAgentTest');
+        $agentTest->setMdp($this->encoder->encodePassword($agentTest, 'mdpAgentTest'));
+        $agentTest->setNom('nomAgentTest');
+        $agentTest->setPrenom('prenomAgentTest');
+		$agentTest->setAdresse('AdresseAgentTest');
+        $manager->persist($agentTest);
+
+
+        $adminTest = new Agent();
+        $adminTest->setLogin('loginAdminTest');
+        $adminTest->setMdp($this->encoder->encodePassword($adminTest, 'mdpAdminTest'));
+        $adminTest->setNom('nomAdminTest');
+        $adminTest->setPrenom('prenomAdminTest');
+		$adminTest->setAdresse('AdresseAdminTest');
+        $manager->persist($adminTest);
+
+
+        $agentAffAgence2 = new Agent();
+        $agentAffAgence2->setLogin('agentAffAgence2');
+        $agentAffAgence2->setMdp($this->encoder->encodePassword($agentAffAgence2, 'agentAffAgence2'));
+        $agentAffAgence2->setNom('agentAffAgence2');
+        $agentAffAgence2->setPrenom('agentAffAgence2');
+		$agentAffAgence2->setAdresse('agentAffAgence2');
+        $manager->persist($agentAffAgence2);
+
+        $adminGene = new Agent();
+        $adminGene->setLogin('testAdminGene');
+        $adminGene->setMdp($this->encoder->encodePassword($adminGene, 'testAdminGene'));
+        $adminGene->setNom('testNom');
+        $adminGene->setPrenom('testPrenom');
+		$adminGene->setAdresse('testAdresse');
+        $manager->persist($adminGene);
+
+        $agentAff = new Agent();
+        $agentAff->setLogin('testAgentAff');
+        $agentAff->setMdp($this->encoder->encodePassword($agentAff, 'testAgentAff'));
+        $agentAff->setNom('testNom');
+        $agentAff->setPrenom('testPrenom');
+		$agentAff->setAdresse('testAdresse');
+        $manager->persist($agentAff);
+
+
+        $manager->flush();
+        
+
+        
     }
 }
